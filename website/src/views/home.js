cade9913d208f57d087f0dd91d67d4dadb5f91e4
Connor Bernard <connorbernard@berkeley.edu>
2024-10-27 18:42:26 -0700

    feat: remove grade projections

--- website/src/views/home.js
+++ website/src/views/home.js
@@ -1,16 +1,16 @@
-import React, { useMemo, useContext, useEffect, useState } from 'react';
-import { Box, Typography, useMediaQuery } from '@mui/material';
+import React, { useMemo, useContext } from 'react';
+import { Box, useMediaQuery } from '@mui/material';
 import useFetch from '../utils/useFetch';
 import Loader from '../components/Loader';
 import GradeAccordion from '../components/GradeAccordion';
 import GradeGrid from '../components/GradeGrid';
 import Grid from '@mui/material/Unstable_Grid2';
-import ProjectionTable from '../components/ProjectionTable';
+// import ProjectionTable from '../components/ProjectionTable';
 import { StudentSelectionContext } from "../components/StudentSelectionWrapper";
 
 function Home() {
 
-    const [binsData, setBinsData] = useState([]);
+    // const [binsData, setBinsData] = useState([]);
 
     const mobileView = useMediaQuery('(max-width:600px)');
 
@@ -20,17 +20,17 @@ function Home() {
         return selectedStudent || localStorage.getItem('email');
     }, [selectedStudent]);
 
-    const binsInfo = useFetch('/bins');
+    // const binsInfo = useFetch('/bins');
     const gradeInfo = useFetch(`students/${fetchEmail}/grades`);
-    const projectionsInfo = useFetch(`/students/${fetchEmail}/projections`);
+    // const projectionsInfo = useFetch(`/students/${fetchEmail}/projections`);
 
-    useEffect(() => {
-        if (binsInfo.data && localStorage.getItem('token')) {
-            setBinsData(binsInfo.data.map(({ letter, points }) => [points, letter]));
-        }
-    }, [binsInfo.data]);
+    // useEffect(() => {
+    //     if (binsInfo.data && localStorage.getItem('token')) {
+    //         setBinsData(binsInfo.data.map(({ letter, points }) => [points, letter]));
+    //     }
+    // }, [binsInfo.data]);
 
-    if (gradeInfo.loading || binsInfo.loading || projectionsInfo.loading) {
+    if (gradeInfo.loading /*|| binsInfo.loading || projectionsInfo.loading */) {
         return (<Loader />);
     }
 
@@ -59,14 +59,14 @@ function Home() {
                     </Grid>
                 </Box>
             }
-            {localStorage.getItem('token') &&
+            {/* {localStorage.getItem('token') &&
                 <Box>
                     <Typography variant='h5' component='div' sx={{ mt: 6, mb: 2, fontWeight: 500, textAlign: 'center' }}>Grade Projections</Typography>
                     <Box sx={{ mb: 4, display: 'flex', flexBasis: 'min-content', justifyContent: 'center' }}>
                         <ProjectionTable projections={projectionsInfo.data} gradeData={binsData} />
                     </Box>
                 </Box>
-            }
+            } */}
         </Box>
     );
 }

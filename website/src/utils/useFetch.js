4188b7a280612a69f9e72772569b9e8cf43636a2
Connor Bernard <connorbernard@berkeley.edu>
2024-11-04 03:07:32 -0800

    feat: add payload body to useFetch

--- website/src/utils/useFetch.js
+++ website/src/utils/useFetch.js
@@ -4,20 +4,23 @@ import apiv2 from './apiv2';
 /**
  * Hook to make API calls.
  * @param {string} stub the stub for the api endpoint you want to hit.
+ * @param {payload} the payload you wish to be sent in the request.
  * @param {String} method the HTTP method you wish to use (GET, POST, PUT...).
  * @param {AxiosInstance} api the axios api you wish to use.
  * @returns {object} values for loading, data, and error.
  */
 export default function useFetch(
     stub,
-    { method, api } = { method: 'GET', api: apiv2 },
+    payload = null,
+    method = 'GET',
+    api = apiv2,
 ) {
     const [data, setData] = useState(undefined);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(undefined);
 
     useEffect(() => {
-        api[method.toLowerCase()](stub)
+        api[method.toLowerCase()](stub, payload)
             .then((res) => {
                 setData(res.data);
             })

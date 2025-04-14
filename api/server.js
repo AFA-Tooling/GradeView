e3562a150cc874b214d28612828ef38f40c9d9b0
TeetoToast <122845522+TeetoToast@users.noreply.github.com>
2025-04-14 16:35:05 -0700

    Added TM to project 1 title to match HAID sheet (#38)

--- api/server.js
+++ api/server.js
@@ -4,55 +4,26 @@ import logger from './lib/logger.mjs';
 import esMain from 'es-main';
 import express, { json } from 'express';
 import ApiV2Router from './Router.js';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { createClient } from 'redis';
-import RateLimit from 'express-rate-limit';
 
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 const PORT = process.env.PORT || 8000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const redisClient = createClient({ url: process.env.REDIS_URL || `redis://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}` });
-redisClient.connect().catch(console.error);
-
-async function metaEndpoint(req, res) {
-  try {
-    const metaData = await redisClient.get('metaData');
-    if (!metaData) {
-      return res.status(404).send('Meta data not found');
-    }
-    res.send(metaData);
-  } catch (error) {
-    console.error("Error fetching meta data from Redis:", error);
-    res.status(500).send("Error fetching meta data");
-  }
-}
-
-const limiter = RateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // max 100 requests per windowMs
-});
 
 async function main() {
-  const app = express();
-  app.use(logger);
-  app.use(cors());
-  app.use(json());
+    const app = express();
+    app.use(logger);
+    app.use(cors());
+    app.use(json());
 
-  app.use('/api', ApiV2Router);
-  // app.get('/api/meta', metaEndpoint);
-  app.get('/api/meta', limiter, metaEndpoint);
+    app.use('/api', ApiV2Router);
+    // Initialize middleware
 
-
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-    console.log('Press Ctrl+C to quit.');
-  });
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`);
+        console.log('Press Ctrl+C to quit.');
+    });
 }
 
+// Run the main function if this is a main module
 if (esMain(import.meta)) {
-  main();
+    main();
 }
